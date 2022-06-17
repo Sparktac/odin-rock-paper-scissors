d92086b0e54c301e47cffe8f99f9a91f75c028c1
Dan S <dspark0609@gmail.com>
2022-06-17 13:01:51 -0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # odin-rock-paper-scissors
+Odin - Rock, Paper, Scissors game
+
+Learning goals:
+- Create a rock, paper, scissors game to be played in DevTools console
+- Utilize Javascript 
+- Use of let, const, function, and more
+- No HTML doc needed atm
--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+console.log('Welcome friend. Let\'s play Rock, Paper, Scissors!');
+
+function computerPlay() {
+    return play[Math.floor(Math.random()*play.length)];
+}
+
+let play = ['rock', 'paper', 'scissors'];
+console.log(computerPlay(play));
+
+function singleRound(playerSelection, computerSelection) {
+    return 
+}
